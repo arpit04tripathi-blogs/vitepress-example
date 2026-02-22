0a282792c4980562e7bb55471afb4290ad881e50
Arpit Tripathi <arpit04tripathi@gmail.com>
2026-02-22 03:18:09 +0000

    register YouTubeEmbed

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,6 +4,8 @@ import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import { initComponent } from 'vitepress-mermaid-preview/component';
 import 'vitepress-mermaid-preview/dist/index.css';
+import { YouTubeEmbed } from '@miletorix/vitepress-youtube-embed' // [!code ++]
+import '@miletorix/vitepress-youtube-embed/style.css' // [!code ++]
 import './style.css'
 
 export default {
@@ -14,6 +16,7 @@ export default {
     })
   },
   enhanceApp({ app, router, siteData }) {
+    app.component('YouTubeEmbed', YouTubeEmbed);
     initComponent(app);
   }
 } satisfies Theme
