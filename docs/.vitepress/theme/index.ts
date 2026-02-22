8e264572b093843b4c2a5420d61a258b29ab3893
Arpit Tripathi <arpit04tripathi@gmail.com>
2026-02-22 20:56:00 +0000

    Default Layout Slots

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -7,12 +7,16 @@ import 'vitepress-mermaid-preview/dist/index.css';
 import { YouTubeEmbed } from '@miletorix/vitepress-youtube-embed' // [!code ++]
 import '@miletorix/vitepress-youtube-embed/style.css' // [!code ++]
 import './style.css'
+import AsideElementComponent from '../../components/AsideElementComponent.vue';
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      'aside-outline-before': () => h(AsideElementComponent),
+      'aside-ads-before': () => h(AsideElementComponent),
+      'aside-ads-after': () => h(AsideElementComponent),
     })
   },
   enhanceApp({ app, router, siteData }) {
